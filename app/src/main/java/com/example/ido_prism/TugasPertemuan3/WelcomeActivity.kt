5e7dcedeb4911ff772f2be8229fdd51d28b36dba
Ido <idosiregar2006@gmail.com>
2026-04-03 17:25:53 +0700

    Update Weclome Activity

--- app/src/main/java/com/example/ido_prism/TugasPertemuan3/WelcomeActivity.kt
+++ app/src/main/java/com/example/ido_prism/TugasPertemuan3/WelcomeActivity.kt
@@ -6,16 +6,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.ido_prism.R
+import com.example.ido_prism.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_welcome)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val username = intent.getStringExtra("USERNAME") ?: "Pengguna"
+
+        binding.tvWelcome.text = "Selamat Datang, $username"
     }
 }
+
