0c809f65e1ce570fb41c5bd1f8128cec2ec7040f
Ido <idosiregar2006@gmail.com>
2026-04-11 00:34:38 +0700

    Tugas Pertemuan 4

---
+++ app/src/main/java/com/example/ido_prism/TugasPertemuan4/DashboardOrangTuaActivity.kt
@@ -0,0 +1,22 @@
+package com.example.ido_prism.TugasPertemuan4
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ido_prism.R
+
+class DashboardOrangTuaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_dashboard_orang_tua)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
