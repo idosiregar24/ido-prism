ef44af3a149b3a7ae34e016f92bdce7f3ef6d501
Ido <idosiregar2006@gmail.com>
2026-04-29 20:57:42 +0700

    Tugas Pertemuan 6: Implementasi BaseActivity, Bottom Navigation, dan Update Splash Screen

---
+++ app/src/main/java/com/example/ido_prism/BaseActivity.kt
@@ -0,0 +1,58 @@
+package com.example.ido_prism
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.ido_prism.Home.HomeFragment
+import com.example.ido_prism.About.AboutFragment
+import com.example.ido_prism.Profile.ProfileFragment
+import com.example.ido_prism.databinding.ActivityBaseBinding
+
+class BaseActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBaseBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityBaseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // Set bottom padding to 0 to avoid gap below BottomNavigation
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
+
+        // Default Fragment
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.nav_about -> {
+                    replaceFragment(AboutFragment())
+                    true
+                }
+                R.id.nav_profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
